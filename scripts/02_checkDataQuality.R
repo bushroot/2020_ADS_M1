a5eb60620b48f197c312714194822d27a11dfcd0
Roger Bär <rogerbaer@Rogers-MacBook-Pro-2.local>
2020-01-29 00:14:11 +0100

    update scripts

--- scripts/02_checkDataQuality.R
+++ scripts/02_checkDataQuality.R
@@ -1,7 +1,4 @@
 
-
-library(codebook)
-
 rmarkdown::render(input = "scripts/02_checkDataQuality.Rmd", 
                   output_dir = "reports/", 
                   output_file = "02_checkDataQuality.html")
